5dc5599140f0a247cc04cc730be6bc4ff4580aca
charltonaustin <charlton.austin@kin.com>
2024-06-22 15:27:47 -0700

    Adding in problem 01.21

---
+++ 01.21.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(printf "smallest-divisor 199: ~a~n" (smallest-divisor 199))
+(printf "smallest-divisor 1999: ~a~n" (smallest-divisor 1999))
+(printf "smallest-divisor 19999: ~a~n" (smallest-divisor 19999))
